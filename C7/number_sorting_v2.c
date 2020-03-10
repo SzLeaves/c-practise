a766dabb6a96301613662cc0a1cc97d504d97327
Szhou <steven-zh@foxmail.com>
2020-03-10 09:42:49 +0800

    Add number sorting version2

---
+++ C7/number_sorting_v2.c
@@ -0,0 +1,26 @@
+//另外一种倒序输出字符串的方式（数组遍历输出）
+#include<stdio.h>
+#include<string.h>
+#define LEN 30
+
+int main(void) {
+	char str[LEN];
+	puts("请输入一个数字：");
+	scanf("%s", str);
+	int length = strlen(str);  //得到字符串的长度 
+	printf("\n");
+	
+	//正序输出
+	puts("正序输出");
+	for (int i = 0; i < length; i++)
+		printf("%c", str[i]);
+		
+		printf("\n\n");
+	
+	//倒序输出
+	puts("倒序输出");	
+	for (int i = length; i >= 0; i--)
+		printf("%c", str[i]);
+	
+	return 0;
+}
